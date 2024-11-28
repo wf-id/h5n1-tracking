f6d703ae4f4c165c09d6e1204e1a82b987098660
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-11-28 10:00:21 -0500

    update for ww tracking

--- scrape.R
+++ scrape.R
@@ -19,3 +19,19 @@ table_dt_long[, UpdateDTS := Sys.time()]
 
 # write to disk
 fwrite(table_dt_long, here("data-raw", "human_cases.csv"), append = TRUE)
+
+# Get WW data
+
+url <- "https://www.cdc.gov/wcms/vizdata/NCEZID_DIDRI/FluA/H5N1Table.json"
+library(httr)
+tmp <- tempfile()
+httr::GET(url, write_disk(tmp))
+
+dat <- jsonlite::fromJSON(tmp)
+
+dat$UpdateDTS <- Sys.time()
+
+dat_long <- dat |>
+  tidyr::gather(DateDT, StatusDT, -Sewershed, -`State/Territory`, -County, -UpdateDTS)
+
+fwrite(dat_long, here("data-raw", "ww_cases.csv"), append = TRUE)
